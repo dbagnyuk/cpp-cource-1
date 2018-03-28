d3ed671b5281e8b4f8869579f947a234a3719d16
dbagnyuk <37371286+dbagnyuk@users.noreply.github.com>
2018-03-28 10:50:43 +0200

    Create cw_task_03.cpp

---
+++ June_16/cw_task_03.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Enter " << endl;
+    int nSize;
+    cin >> nSize;
+
+    cout << endl;
+
+    int nStars = nSize;
+    for (int i = 0; i < nSize; ++i)
+    {
+        for (int j = 0; j < nStars; ++j)
+            cout << "*";
+        --nStars;
+
+
+        cout << endl;
+    }
+    cout << endl;
+
+    return 0;
+}
