3ff8c0dda540336dbac1f68dcf008a1d09faff4c
dbagnyuk <37371286+dbagnyuk@users.noreply.github.com>
2018-04-13 14:28:22 +0200

    Update Pointers_Array-cw_2.cpp

--- July_05/cw_task_03/Pointers_Array-cw_2.cpp
+++ July_05/cw_task_03/Pointers_Array-cw_2.cpp
@@ -13,7 +13,7 @@ using namespace std;
 int main()
 {
 	const int nSize = 10;
-	int iArray[nSize] = { 10,20,30,40,50,60,70,80,90,95};
+	int iArray[nSize] = { 10,20,30,40,50,60,70,80,90,95 };
 
 	cout << iArray << endl;
 
