273b62529d1597462f23094008986deafa20fe73
dbagnyuk <37371286+dbagnyuk@users.noreply.github.com>
2018-04-13 14:55:04 +0200

    Add files via upload
    array figure with 0 and 1

---
+++ July_12/hw_task_01_d/stdafx.cpp
@@ -0,0 +1,8 @@
+// stdafx.cpp : source file that includes just the standard includes
+// Array-filled-by-0-1_hw1-d.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
+
+#include "stdafx.h"
+
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
